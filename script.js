46596a8417e347b1fc677a177fa73dc416fdfcc6
Jeric <jericsmir@gmail.com>
2023-12-31 12:27:56 -0800

    figured out javascript layout

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
-const number = document.getElementById('number');
+const number = document.getElementById('number1');
+const number2 = document.getElementById('num');
 const generator = document.getElementById('generatebtn');
 
 const randomNumberGenerator = () => {
-    const randomNumber = Math.floor(Math.random() * 1000000 / 10 + 1);
+    const randomNumber = Math.floor(Math.random() * 10 * 3);
     number.textContent = randomNumber;
 }
 
+const randomNumberGenerator2 = () => {
+    const randomNumber = Math.floor(Math.random() * 10000 / 2);
+    number2.textContent = randomNumber;
+}
+
+
 generatebtn.addEventListener('click', randomNumberGenerator)
+generatebtn.addEventListener('click', randomNumberGenerator2)
 
 
 randomNumberGenerator;
+randomNumberGenerator2;
